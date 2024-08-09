fe83fa684ecefe7cf70201144c14fc99d241358e
Roooo317 <167042061+Roooo317@users.noreply.github.com>
2024-08-09 19:20:07 +0900

    初回コミット

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(function () {
+
+  //ページ内スクロール
+  var navHeight = $(".header").outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate({ scrollTop: 0, }, 300);
+    return false;
+  });
+
+   // スクロールしたときにセクションをフェードインさせる
+   $(window).scroll(function () {
+    const scrollAmount = $(window).scrollTop();
+    const windowHeight = $(window).height();
+    $('section').each(function () {
+      const position = $(this).offset().top;
+      if (scrollAmount > position - windowHeight + 100) {
+        $(this).addClass('fade-in');
+      }
+    });
+  });
+  
+
+});
